ee9b835200f40e07f249ef97b3471fad6e0c06dd
sandeep <sandeeppatil10705@gmail.com>
2025-07-16 22:14:06 +0530

    Update contact with X

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,16 +12,25 @@ export default function Contact() {
     offset: ["start end", "end start"],
   })
 
-  const y = useTransform(scrollYProgress, [0, 1], [100, -100])
-  const opacity = useTransform(scrollYProgress, [0, 0.2, 0.9, 1], [0, 1, 1, 0])
+
+const XIcon = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 50 50"
+    fill="currentColor"
+    className={className}
+  >
+    <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
+  </svg>
+);
 
   const contactList = [
     { icon: Mail, title: "Email", content: "sandeeppatil10705@gmail.com", href: "mailto:sandeeppatil10705@gmail.com" },
-    { icon: Send , title: "Telegram", content: "@tgtsandeep", href: "https://t.me/tgtsandeep" },
-    { icon: Github, title: "GitHub", content: "@sandeep-m-patil", href: "https://github.com/sandeep-m-patil" },
-    { icon: Linkedin, title: "LinkedIn", content: "@psandeep2005", href: "https://linkedin.com/in/psandeep2005" },
-    { icon: Code2, title: "LeetCode", content: "@23ag1a66b7", href: "https://leetcode.com/23ag1a66b7" },
-    { icon: TerminalSquare, title: "HackerRank", content: "@23ag1a66b7", href: "https://www.hackerrank.com/23ag1a66b7" },
+    { icon: XIcon, title: "X", content: "SANDEEPMADHAV2", href: "https://x.com/SANDEEPMADHAV2" },
+    { icon: Github, title: "GitHub", content: "sandeep-m-patil", href: "https://github.com/sandeep-m-patil" },
+    { icon: Linkedin, title: "LinkedIn", content: "psandeep2005", href: "https://linkedin.com/in/psandeep2005" },
+    { icon: Code2, title: "LeetCode", content: "23ag1a66b7", href: "https://leetcode.com/23ag1a66b7" },
+    { icon: TerminalSquare, title: "HackerRank", content: "23ag1a66b7", href: "https://www.hackerrank.com/23ag1a66b7" },
   ]
 
   return (
